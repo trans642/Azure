897e88539b2fa327e0ba7658ae5335869620972b
Tro Taslakian <tro_t@hotmail.com>
2015-06-30 12:49:23 -0400

    view options layout was set to true

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ console.log('test from server');
 // view engine setup
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
-app.set('view options', { layout: false });
+app.set('view options', { layout: true });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
